99bbee550a4b8c32ac09971ebab471b7e02de38a
RyanmjDev <Ryanmj.dev@gmail.com>
2023-12-06 13:39:57 -0600

    Add dependencies and login route

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const ChatMessage = require('./models/ChatMessage');
 
+
+
 const axios = require('axios')
 
 require('dotenv').config();
@@ -44,6 +46,44 @@ app.post('/', (req, res) => {
         isUser: req.body.isUser,
      })
 })
+
+// Route for logging
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if the email and password are provided
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required' });
+    }
+
+    // Find the user in the database based on the email
+    const user = await User.findOne({ email });
+
+    // Check if the user exists
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // Generate a JWT token for authentication
+    const token = generateToken(user);
+    console.log("Successfully login!")
+    res.json({ token });
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(500).json({ message: 'Error logging in' });
+  }
+});
+
+
 
 app.post('/chat', async (req, res) => {
   try {
